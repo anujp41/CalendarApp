7eeeb4fb2c0fb9751754b7d928503c1bad4971ec
anujp41 <anuj.pant41@gmail.com>
2018-06-21 16:39:06 -0400

    first commit

---
+++ CalendarBackend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require("path");
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', require('./api'));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
---
+++ CalendarFrontEnd/app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('here')
+
+ReactDOM.render(
+  <div>Hello, meow!</div>,
+  document.getElementById('app')
+);
