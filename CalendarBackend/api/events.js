5368dd0fb10159c4b5e010d611b3153b4fd323a6
anujp41 <anuj.pant41@gmail.com>
2018-06-26 20:00:48 -0400

    working on querying database for given month

--- CalendarBackend/api/events.js
+++ CalendarBackend/api/events.js
@@ -1,26 +1,36 @@
+const { Op } = require('sequelize');
 const router = require('express').Router();
 const { Events } = require('../models');
 
 //function changes database results from simple JSON to complicated that can be used by front-end
 const updateJSONObject = events => {
   let eventArr = [];
+  console.log('events is ', events)
   if (Array.isArray(events)) {
     eventArr = events.map(event => event.dataValues);
   } else {
     eventArr = [events.dataValues];
   }
-  const eventObj = {};
-  for (let event of eventArr) {
-    eventObj[event.eventDate] ? eventObj[event.eventDate].push(event) : eventObj[event.eventDate] = [event];
-  }
-  return eventObj;
+  return eventArr;
+  // const eventObj = {};
+  // for (let event of eventArr) {
+  //   eventObj[event.eventDate] ? eventObj[event.eventDate].push(event) : eventObj[event.eventDate] = [event];
+  // }
+  // return eventObj;
 }
 
 /* Each of the handlers (except DELETE) call the updateJSONObject function above
     to modify the return JSON value in a JSON value with different key value */
 // GET request handler
 router.get('/', function (req, res, next) {
-  Events.findAll()
+  Events.findAll({
+    where: {
+      eventDate: {
+        [Op.gte]: new Date('2018-06-04'),
+        [Op.lte]: new Date('2018-06-05')
+      }
+    }
+  })
   .then(events => res.json(updateJSONObject(events)))
   .catch(next);
 });
